24ee84d1eeb520beac4ef05e516d1b2b2ff0789a
Lindsay Gilbert <Lindsaygilbert722@gmail.com>
2017-07-08 10:46:24 -0700

    added proptypes

--- js/Search.js
+++ js/Search.js
@@ -6,11 +6,17 @@ const Search = React.createClass({
   render () {
     return (
       <div className='search'>
-        {preload.shows.map(((show) => {
-          return (
-            <ShowCard key={show.imdbID} show={show} />
-          )
-        }))}
+        <header>
+          <h1>svideo</h1>
+          <input type='text' placeholder='Search' />
+        </header>
+        <div>
+          {preload.shows.map((show) => {
+            return (
+              <ShowCard key={show.imdbID} {...show} />
+            )
+          })}
+        </div>
       </div>
     )
   }
--- js/ShowCard.js
+++ js/ShowCard.js
@@ -1,12 +1,16 @@
 import React from 'react'
+const { string } = React.PropTypes
 
 const ShowCard = React.createClass({
+  propTypes: {
+    title: string,
+    year: string,
+    description: string
+  },
   render () {
-    const { poster, title, year, description } = this.props.show
+    const { title, year, description } = this.props.show
     return (
       <div className='show-card'>
-        <img src={`/public/img/posters/${poster}`}
-          />
         <div>
           <h3>{title}</h3>
           <h4>({year})</h4>
