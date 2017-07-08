f323cb764761037c90284d0e9948adefdf1e51ca
Lindsay Gilbert <Lindsaygilbert722@gmail.com>
2017-07-08 11:12:52 -0700

    added search filter and map functionality

--- js/Search.js
+++ js/Search.js
@@ -3,15 +3,26 @@ import ShowCard from './ShowCard'
 import preload from '../public/data.json'
 
 const Search = React.createClass({
+  getInitialState() {
+    return {
+      searchTerm: ''
+    }
+  },
+  handleSearchTermChange (event) {
+    this.setState({searchTerm: event.target.value})
+  },
   render () {
     return (
       <div className='search'>
         <header>
           <h1>svideo</h1>
-          <input type='text' placeholder='Search' />
+          <input onChange={this.handleSearchTermChange} value={this.state.searchTerm} type='text' placeholder='Search' />
         </header>
         <div>
-          {preload.shows.map((show) => {
+          {preload.shows
+            .filter((show) =>
+              return `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0)
+            .map((show) => {
             return (
               <ShowCard key={show.imdbID} {...show} />
             )
